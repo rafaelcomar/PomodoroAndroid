3de17b4637c54c77845514860a1a86521eb09fd2
rafael comar <rafael.comar@grupogsh.com.br>
2016-10-31 18:27:40 -0300

    adicionando conexão com o banco

--- app/src/main/java/com/example/usuario/pomodoroandroid/dao/TaskDAO.java
+++ app/src/main/java/com/example/usuario/pomodoroandroid/dao/TaskDAO.java
@@ -1,8 +1,12 @@
 package com.example.usuario.pomodoroandroid.dao;
 
+import android.content.ContentValues;
 import android.content.Context;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import com.example.usuario.pomodoroandroid.model.Task;
+import com.example.usuario.pomodoroandroid.util.DbHelper;
 
 import java.util.ArrayList;
 
@@ -14,6 +18,9 @@ public class TaskDAO {
     public static ArrayList<Task> listTasks ;
     private Context context;
     private Task tsk;
+
+    private DbHelper criaBanco;
+    private SQLiteDatabase db;
 
 
 
@@ -35,7 +42,68 @@ public class TaskDAO {
             listTasks.add(t2);
 
         }
+
+        if (criaBanco == null){
+            criaBanco = new DbHelper(context);
+            carregaDados();
+        }else{
+            carregaDados();
+        }
+
+
     }
+
+    public String inserirTask(Task task){
+        ContentValues valores;
+        long resultado;
+
+        db = criaBanco.getWritableDatabase();
+        valores = new ContentValues();
+        valores.put(criaBanco.title , task.getTitle());
+        valores.put(criaBanco.description, task.getDescription());
+        valores.put(criaBanco.qtdSegundos, task.getQtdSegundos());
+        valores.put(criaBanco.qtdPomodoros, task.getQtdPomodoros());
+        valores.put(criaBanco.pomodorosFeitos, task.getPomodorosFeitos());
+        valores.put(criaBanco.concluded, task.isConcluded());
+        valores.put(criaBanco.descanso, task.isDescanso());
+
+        resultado = db.insert(criaBanco.tabela, null, valores);
+        db.close();
+
+        if (resultado ==-1)
+            return "Erro ao inserir registro";
+        else
+            return "Registro Inserido com sucesso";
+
+    }
+
+    public Cursor carregaDados(){
+        Cursor cursor;
+        String[] campos =  {criaBanco.title ,criaBanco.description ,criaBanco.qtdSegundos, criaBanco.qtdPomodoros,
+                criaBanco.pomodorosFeitos,criaBanco.concluded, criaBanco.descanso};
+        db = criaBanco.getReadableDatabase();
+        cursor = db.query(criaBanco.tabela, campos, null, null, null, null, null, null);
+
+        if(cursor!=null){
+            cursor.moveToFirst();
+            while (cursor.moveToNext()){
+                Task t = new Task();
+                t.setTitle(cursor.getString(0));
+                t.setDescription(cursor.getString(1));
+                t.setQtdSegundos(cursor.getString(2));
+                t.setQtdPomodoros(cursor.getString(3));
+                t.setPomodorosFeitos(Integer.parseInt(cursor.getString(4)));
+                t.setConcluded(Boolean.parseBoolean(cursor.getString(5)));
+                t.setDescanso(Boolean.parseBoolean(cursor.getString(6)));
+
+                listTasks.add(t);
+            }
+        }
+
+        db.close();
+        return cursor;
+    }
+
 
     public static ArrayList<Task> getListTasks() {
         return listTasks;
---
+++ app/src/main/java/com/example/usuario/pomodoroandroid/util/DbHelper.java
@@ -0,0 +1,56 @@
+package com.example.usuario.pomodoroandroid.util;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by rafael.comar on 31/10/2016.
+ */
+public class DbHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "PomodoroAndroid.db";
+    String SQL_CREATE_ENTRIES;
+
+    public static String tabela = "tasks";
+    public static String title = "titulo";
+    public static  String description = "descricao";
+    public static  String qtdPomodoros = "total_pomodoros";
+    public static  String qtdSegundos = "segundos_restantes";
+    public static  String descanso="descanso";
+    public static  String concluded="concluido";
+    public static  String  pomodorosFeitos="pomodoros_feitos";
+
+
+
+//    public DbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+//        super(context, name, factory, version);
+//    }
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME , null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        SQL_CREATE_ENTRIES = "CREATE " + tabela +" tasks(" +
+                " ID integer primary key autoincrement,"
+                + title + "text,"
+                + description + "text,"
+                + qtdPomodoros + "text"
+                + qtdSegundos + "text"
+                + descanso + "integer"
+                + concluded + "integer"
+                + pomodorosFeitos + "integer"
+                +")";
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS tasks");
+        onCreate(db);
+    }
+}
